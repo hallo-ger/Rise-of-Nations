5a0ed89097b0d38cd5823b0f3c699933a63bb634
hallo-ger <alex@turnbow.de>
2020-03-21 21:17:22 +0100

    Initial commit

--- common/defines/More_Divisions_Per_Commander.lua
+++
@@ -1,4 +0,0 @@
-NDefines.NMilitary.CORPS_COMMANDER_DIVISIONS_CAP = 30			-- how many divisions a corps commander is limited to. 0 = inf, < 0 = blocked
-NDefines.NMilitary.CORPS_COMMANDER_ARMIES_CAP = -1				-- how many armies a corps commander is limited to. 0 = inf, < 0 = blocked
-NDefines.NMilitary.FIELD_MARSHAL_DIVISIONS_CAP = 30			-- how many divisions a field marshall is limited to. 0 = inf, < 0 = blocked
-NDefines.NMilitary.FIELD_MARSHAL_ARMIES_CAP = 5					-- how many armies a field marshall is limited to. 0 = inf, < 0 = blocked
--- descriptor.mod
+++
@@ -1,7 +0,0 @@
-version="1"
-tags={
-	"Gameplay"
-}
-name="Rise of Nations"
-picture="thumbnail.png"
-supported_version="1.8.2"
